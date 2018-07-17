65c5dd619a1b5f6a6f223faf710ffe3518ad350f
user1 <user1@user1s-iMac.local>
2018-07-17 02:43:02 -0700

    create getters and setters for books

---
+++ src/Books.java
@@ -0,0 +1,70 @@
+public class Books {
+    private int isbn;
+    private int pages;
+    private String author;
+    private String publisher;
+    private String genre;
+    private String pubDate;
+
+    // set isbn
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    // get isbn
+    public int getIsbn() {
+        return this.isbn;
+    }
+
+    // set pages
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    // get pages
+    public int getPages() {
+        return this.pages;
+    }
+
+    // set author
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    //get author
+    public String getAuthor() {
+        return this.author;
+    }
+
+    //set publisher
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    //get publisher
+    public String getPublisher() {
+        return this.publisher;
+    }
+
+    //set genre
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    //get genre
+    public String getGenre() {
+        return this.genre;
+    }
+
+    //set pubdate
+    public void setPubDate(String date) {
+        this.pubDate = date;
+    }
+
+    //get pubdate
+    public String getPubDate() {
+        return this.pubDate;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Books HarryPotter = new Books();
+        HarryPotter.setAuthor("JK Rowling");
+        System.out.println(HarryPotter.getAuthor());
+    }
+}
