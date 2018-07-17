45641d988daa481fe2d8ca06cdcfb5f1366201af
MacBruce <brucemac1992@gmail.com>
2018-07-17 16:48:08 -0700

    add static CheckOut class, pls impl CheckOutHistory

---
+++ src/CheckOut.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CheckOut {
+    // see if currently checkedout
+    private static boolean isCheckedout;
+    // dynamic arr with dict, when & who
+    private static ArrayList<HashMap<String, String>> CheckOutHistory;
+    private static String dateCheckedOutLast;
+    private static String whoCheckedOutLast;
+
+
+
+
+}
