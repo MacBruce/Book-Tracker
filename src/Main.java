2d7e11e63024c2806303bf960bdadcdf6c164692
user1 <user1@user1s-iMac.local>
2018-07-17 02:48:08 -0700

    left todo on main regarding method chain

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@
 public class Main {
 
     public static void main(String[] args) {
+        // testing
         Books HarryPotter = new Books();
         HarryPotter.setAuthor("JK Rowling");
         System.out.println(HarryPotter.getAuthor());
+
+        /* todo consider using method chaining on the books class */
     }
 }
