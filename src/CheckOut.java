3b4d4ccc6f2b6c98f8613307e98fc418eef315f0
Mac <brucemac1992@gmail.com>
2018-07-17 20:05:36 -0700

    set date of checkOut, next getter

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -4,10 +4,21 @@ import java.util.HashMap;
 public class CheckOut {
     // see if currently checkedout
     private static boolean isCheckedout;
-    // dynamic arr with dict, when & who
-    private static ArrayList<HashMap<String, String>> CheckOutHistory;
+    // dynamic arr with dict, when & who checked out
+    private static ArrayList<HashMap<String, String>> CheckOutHistory = new ArrayList<>();
     private static String dateCheckedOutLast;
     private static String whoCheckedOutLast;
+
+    public static void checkBookOut(HashMap<String, String> item) {
+        isCheckedout = true;
+        CheckOutHistory.add(item);
+    }
+
+    public static void setDateCheckedOutLast(){
+        int lengthOfHistory = CheckOutHistory.size();
+        dateCheckedOutLast = CheckOutHistory.get(lengthOfHistory - 1).toString();
+    }
+
 
 
 
