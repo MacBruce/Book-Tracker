cc5080779defc084c20111526810ab925fd29c75
MacBruce <brucemac1992@gmail.com>
2018-07-17 22:52:30 -0700

    created Checkout class to check book availability and history

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -3,20 +3,30 @@ import java.util.HashMap;
 
 public class CheckOut {
     // see if currently checkedout
-    private static boolean isCheckedout;
+    private static boolean isCheckedOut;
     // dynamic arr with dict, when & who checked out
     private static ArrayList<HashMap<String, String>> CheckOutHistory = new ArrayList<>();
     private static String dateCheckedOutLast;
     private static String whoCheckedOutLast;
 
     public static void checkBookOut(HashMap<String, String> item) {
-        isCheckedout = true;
-        CheckOutHistory.add(item);
+        //if hashmap is not empty
+        if (item.size() > 0) {
+            isCheckedOut = true;
+            CheckOutHistory.add(item);
+        }
+        // else show error
+
+
     }
 
     public static void setDateCheckedOutLast(){
         int lengthOfHistory = CheckOutHistory.size();
         dateCheckedOutLast = CheckOutHistory.get(lengthOfHistory - 1).toString();
+    }
+
+    public static String getDateCheckedOutLast() {
+        return "";
     }
 
 
